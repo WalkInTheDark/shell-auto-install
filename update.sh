fa00a9ea1e5ddc525a23c5076517afc857de9f69
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-14 11:55:46 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,22 +6,27 @@ load() {
         source lib/$i
     done
 }
+
+xin() {
 test_install git
 
 test_www www.baidu.com
 
-
 a=`pwd`
 b=`echo ${a##*/}`
-
 ver=`cat conf/version.txt`
 new_ver=`https://raw.githubusercontent.com/goodboy23/shell-auto-install/master/conf/version.txt`
+c=`process $ver $new_var`
 
-if [ ver -lt $new_ver ];then
+if [ "$c" == "$new_ver" ];then
    cd ..
    tar -cf old.tar.gz $b
+   rm -rf $b
    git clone https://github.com/goodboy23/shell-auto-install.git
    [ -d shell-auto-install ] && echo "ok" || echo "error"
 else
     echo "ok"
 fi
+}
+
+xin
