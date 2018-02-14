e45c6fcebdd5184b67f3b1324a150297a0f1c6aa
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-14 11:43:13 +0800

    Update sai.sh

--- sai.sh
+++ sai.sh
@@ -3,11 +3,26 @@ language=`cat conf/lang.txt`
 
 
 
+master_dir() { #可修改
+    install_dir=/ops/server
+    log_dir=/ops/log
+}
+
 load() {
     for i in `ls lib`
     do
         source lib/$i
     done
+}
+
+lang() {
+    if [ $1 -eq 1 ];then
+        echo "1" > conf/lang.txt
+    elif [ $1 -eq 2 ];then
+        echo "2" > conf/lang.txt
+    else
+        test_exit "选项错误" "Wrong option"
+    fi
 }
 
 help_all() {
@@ -22,19 +37,23 @@ list_all() {
 }
 
 server() {
-    if [ -f script/${2}.sh ];then
-        source script/${2}.sh
+    local a=`process_bian $2`
+
+    if [ -f script/${a}.sh ];then
+        source script/${a}.sh
         
         if [ "$1" == "install" ];then
             test_root
             test_www www.baidu.com
-            install_${2}
+            install_${a}
         elif [ "$1" == "remove" ];then
-            remove_${2}
+            remove_${a}
         elif [ "$1" == "get" ];then
-            get_${2}
+            get_${a}
+        elif [ "$1" == "info" ];then   
+            info_${a}
         elif [ "$1" == "edit" ];then
-            vi script/${2}.sh
+            vi script/${a}.sh
         else
             help_all
         fi
@@ -43,35 +62,32 @@ server() {
     fi
 }
 
-lang() {
-    if [ $1 -eq 1 ];then
-        echo "1" > conf/lang.txt
-    elif [ $1 -eq 2 ];then
-        echo "2" > conf/lang.txt
-    else
-        test_exit "选项错误" "Wrong option"
-    fi
-}
-
 
 
 #主体
 load
+master_dir
 
 if [ "$#" -eq 0 ];then
     help_all
-elif [ "$#" -eq 1 ];then
-    if [ "$1" == "list" ];then
+elif [ "$1" == "list" ];then 
+    if [ $# -eq 1  ];then
         list_all
-    elif [ "$1" == "help" ];then
-        help_all
     else
-        help_all
+        [ $language -eq 1 ] && echo "$2 相关脚本：" || echo "$2 Related script"
+        grep $2 conf/server_name.txt
     fi
-elif [ "$#" -eq 2 ];then
-    if [ "$1" == "lang" ];then
-        lang $2
+elif [ "$1" == "type" ];then
+    if [ $# -eq 1 ];then
+        echo "servre system-man game"
     else
-        server $1 $2
+        [ $language -eq 1 ] && echo "$2 相关类型脚本：" || echo "$2 Related type script"
+        grep -w $2 conf/server_name.txt
     fi
+elif [ "$1" == "help" ];then
+    help_all
+elif [ "$1" == "lang" ];then
+    lang $2
+else
+    server $1 $2
 fi
