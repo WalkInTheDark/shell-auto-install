8190e6f92ddaf152f62e4f0d45cd1483aee0ce90
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-16 18:35:55 +0800

    Update sai.sh

--- sai.sh
+++ sai.sh
@@ -70,8 +70,12 @@ server() {
         if [ "$1" == "install" ];then
             test_root
             test_www www.baidu.com
+            grep "^${a}" conf/installed.txt &> /dev/null
+            [ $? -eq 0 ] && test_exit "${a}已安装" "${a} is already installed"
             install_${a}
         elif [ "$1" == "remove" ];then
+            grep "^${a}" conf/installed.txt &> /dev/null
+            [ $? -eq 0 ] && sed "/^${a}/d" conf/installed.txt
             remove_${a}
         elif [ "$1" == "get" ];then
             get_${a}
