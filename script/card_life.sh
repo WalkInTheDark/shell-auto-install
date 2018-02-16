ef1b45de7c6261948ff54ab531eae38432302eac
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-16 15:09:35 +0800

    Update card_life.sh

--- script/card_life.sh
+++ script/card_life.sh
@@ -28,3 +28,33 @@ info_card_life() {
         echo "use：card-life"
     fi
 }
+
+    if [ $language -eq 1 ];then
+        echo "名字：card-life"
+        echo
+        echo "类型：游戏"
+        echo
+        echo "版本：1.4"
+        echo
+        echo "作者：book"
+        echo
+        echo "介绍：抽卡人生shell版本，你被困在地牢里，目标是逃出去"
+        echo
+        echo "提示：将会生成wiki_book.ttt的一个存档，如果想开挂，修改里面数值即可。"
+        echo
+        echo "使用：card-life进入游戏，输入1-6来购买卡牌"
+    else
+        echo "Name：card-life"
+        echo
+        echo "Type：game"
+        echo
+        echo "version：1.4"
+        echo
+        echo "Author：book"
+        echo
+        echo "Introduction：Shell life shell version, you are trapped in the dungeon, the goal is to escape"
+        echo
+        echo "Prompt：Will generate an archive of wiki_book.ttt, if you want to hang up, you can modify the value inside."
+        echo
+        echo "use：card-life into the game, enter 1-6 to buy cards"
+    fi
