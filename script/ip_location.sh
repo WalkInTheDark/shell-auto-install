59cf022e6594e6d28e08c6a00b8dcfb8ad7c97a2
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-16 18:55:42 +0800

    Update ip_location.sh

--- script/ip_location.sh
+++ script/ip_location.sh
@@ -20,34 +20,22 @@ remove_ip_location() {
 
 info_ip_location() {
     if [ $language -eq 1 ];then
-        echo "名字：ip-location"
-        echo
-        echo "类型：系统"
-        echo
-        echo "版本：1.0"
-        echo
-        echo "作者：book"
-        echo
-        echo "介绍：查询当前外网ip所在地"
-        echo
-        echo "提示：需要可以上外网"
-        echo
-        echo "使用：ip-location -i 8.8.8.8 来查询这个ip的外网地址"
-        echo "      ip-location -q         查询尝试登陆此服务器的ip"
+        echo "名字：ip-location
+类型：系统
+版本：1.0
+作者：book
+介绍：查询当前外网ip所在地
+提示：需要可以上外网
+使用：ip-location -i 8.8.8.8 来查询这个ip的外网地址
+      ip-location -q         查询尝试登陆此服务器的ip"
     else
-        echo "Name：ip-location"
-        echo
-        echo "Type：sys"
-        echo
-        echo "version：1.0"
-        echo
-        echo "Author：book"
-        echo
-        echo "Introduction：Query current ip network location"
-        echo
-        echo "Prompt：Need to be on the Internet"
-        echo
-        echo "use：ip-location -i 8.8.8.8 to query the IP's external network address"
-        echo "     ip-location-q query ip attempt to log in to this server"
+        echo "Name：ip-location
+Type：sys
+version：1.0
+Author：book
+Introduction：Query current ip network location
+Prompt：Need to be on the Internet
+use：ip-location -i 8.8.8.8 to query the IP's external network address
+     ip-location-q query ip attempt to log in to this server"
     fi
 }
