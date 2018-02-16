71bfbe8cf6a0730540db805a9f35a46dad7c9029
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-16 17:51:34 +0800

    Update pxe.sh

--- script/pxe.sh
+++ script/pxe.sh
@@ -20,12 +20,34 @@ remove_pxe() {
 
 info_pxe() {
     if [ $language -eq 1 ];then
-        echo "功能：pxe一键装机，交互"
+        echo "名字：pxe"
         echo
-        echo "使用：pxe"
+        echo "类型：服务"
+        echo
+        echo "版本：2.0"
+        echo
+        echo "作者：book"
+        echo
+        echo "介绍：pxe一键装机搭建脚本，需要进行一些网段之类的设置"
+        echo
+        echo "提示：无"
+        echo
+        echo "使用：pxe命令进去后，选择网卡和镜像就可以部署了"
+        echo "      支持kisckstart无人值守，要预先搞好配置文件，这里给出文件位置即可"
     else
-        echo "Function: pxe a key installed, interactive"
+        echo "Name：pxe"
         echo
-        echo "use：pxe"
+        echo "Type：server"
+        echo
+        echo "version：2.0"
+        echo
+        echo "Author：book"
+        echo
+        echo "Introduction：pxe installed a script installed script, you need to set up some of the network segment"
+        echo
+        echo "Prompt：none"
+        echo
+        echo "use：pxe command into it, select the network card and image can be deployed"
+        echo "     Support kisckstart unattended, to advance the configuration file, file location can be given here"
     fi
 }
