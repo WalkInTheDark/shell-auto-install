49b93d11ed43b3e900198ab1fb4a5740ed442d7d
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-16 15:13:07 +0800

    Update cha.sh

--- script/cha.sh
+++ script/cha.sh
@@ -28,3 +28,34 @@ info_cha() {
         echo "use：cha"
     fi
 }
+
+    if [ $language -eq 1 ];then
+        echo "名字：cha"
+        echo
+        echo "类型：系统"
+        echo
+        echo "版本：1.0"
+        echo
+        echo "作者：book"
+        echo
+        echo "介绍：显示系统信息"
+        echo
+        echo "提示：无"
+        echo
+        echo "使用：cha命令显示英文系统信息，-c显示中文"
+    else
+        echo "Name：cha"
+        echo
+        echo "Type：sys"
+        echo
+        echo "version：1.0"
+        echo
+        echo "Author：book"
+        echo
+        echo "Introduction：Display system information"
+        echo
+        echo "Prompt：none"
+        echo
+        echo "use：cha command to display English system information, -c display Chinese"
+
+    fi
