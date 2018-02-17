9065c69c3e2073bb8f0503545afbe27b20297f0e
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-17 18:50:09 +0800

    Update cons_mem.sh

--- script/cons_mem.sh
+++ script/cons_mem.sh
@@ -19,19 +19,31 @@ remove_cons_mem() {
 info_cons_mem() {
     if [ $language -eq 1 ];then
         echo "名字：cons-mem
+        
 类型：系统
+
 版本：1.0
+
 作者：LingYi
+
 介绍：监控或消耗内存资源，　指定需要消耗到的百分比
+
 提示：无
+
 使用：直接回车，查看当前内存的百分比，或者输入欲消耗的内存百分比，消耗固定内存"
     else
         echo "Name：cons-mem
+        
 Type：sys
+
 version：1.0
+
 Author：LingYi
+
 Introduction：Monitor or consume memory resources, specifying the percentage that needs to be consumed
+
 Prompt：none
+
 use：Enter directly to see the current percentage of memory, or enter the percentage of memory to consume, consume fixed memory"
     fi
 }
