f33957c8a30da63e43c9dd3b13edf949df9acbea
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-17 18:52:11 +0800

    Update mine.sh

--- script/mine.sh
+++ script/mine.sh
@@ -21,20 +21,32 @@ remove_mine() {
 info_mine() {
     if [ $language -eq 1 ];then
         echo "名字：mine
+        
 类型：游戏
+
 版本：1.0
+
 作者：未知
+
 介绍：扫雷小游戏，可以选择难度
+
 提示：无
+
 使用：mine命令进入后，1-3来选择难度，4退出
      wasd移动，j挖，f放旗子，n返回上一级，x退出"
     else
         echo "Name：mine
+        
 Type：game
+
 version：1.0
+
 Author：unknown
+
 Introduction：minesweeper game, you can choose the difficulty
+
 Prompt：none
+
 use：mine command to enter, 1-3 to choose the difficulty, 4 exit
      wasd move, j dig, f put flag, n return to the previous level, x exit"
     fi
