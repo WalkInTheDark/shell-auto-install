6a25bda495696cfabaae9194f6c798078c50c14c
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-17 18:50:49 +0800

    Update gobang.sh

--- script/gobang.sh
+++ script/gobang.sh
@@ -21,20 +21,32 @@ remove_gobang() {
 info_gobang() {
     if [ $language -eq 1 ];then
         echo "名字：gobang
+        
 类型：游戏
+
 版本：1.0
+
 作者：yinyuemi
+
 介绍：大家熟知的五子棋
+
 提示：人机博弈需要等一段时间，机器需要反应
+
 使用：gobang进入游戏，按1选择人对人，按2选择人机博弈
      jikl移动，空格确定，b返回首页，q退出"
     else
         echo "Name：gobang
+        
 Type：game
+
 version：1.0
+
 Author：yinyuemi
+
 Introduction：We all know the backgammon
+
 Prompt：Man-machine game needs to wait for a period of time, the machine needs to respond
+
 use：Gobang into the game, press 1 to select people, press 2 to select man-machine game
      jikl move, spaces to determine, b back to the home page, q exit"
     fi
