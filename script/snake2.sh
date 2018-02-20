87e5969671a0138d2996656610de58cedcd96991
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-20 17:51:41 +0800

    Create snake2.sh

---
+++ script/snake2.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+get_snake2() {
+    [ $language -eq 1 ] && echo "不用下载" || ehco "Do not download"
+}
+
+install_snake2() {
+    cp conf/alone/snake2 /usr/local/bin/snake2
+    chmod +x /usr/local/bin/snake2
+
+    echo "snake2" >> conf/installed.txt
+    [ $language -eq 1 ] && echo "snake2安装完毕，使用snake2 开始游戏" || ehco "snake2 installation is completed, use snake2 Start the game"
+}
+
+remove_snake2() {
+    rm -rf /usr/local/bin/snake2
+    [ $language -eq 1 ] && echo "snake2已卸载" || ehco "snake2 Uninstalled"
+}
+
+
+info_snake2() {
+    if [ $language -eq 1 ];then
+        echo "名字：snake2
+        
+版本：1.0
+
+作者：liungkejin
+
+介绍：大家熟知的贪吃蛇
+
+提示：无
+
+使用：snake2命令进入游戏，jkil来吃点"
+    else
+        echo "Name：snake2
+        
+version：1.0
+
+Author：liungkejin
+
+Introduction：We all know the snake
+
+Prompt：none
+
+use：snake2 command into the game, jkil move to eat"
+    fi
+}
