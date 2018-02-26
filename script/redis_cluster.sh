4376809c6f343589888c14637c51867e74d15ee2
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-26 09:05:36 +0800

    Update redis_cluster.sh

--- script/redis_cluster.sh
+++ script/redis_cluster.sh
@@ -11,7 +11,10 @@ cluster_ip="127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.
 node=1
 
 
+
+#加载它的依赖
 source script/redis.sh
+
 get_redis_cluster() {
     get_redis
 }
@@ -34,7 +37,7 @@ remove_redis_cluster() {
     if [ $language -eq 1 ];then
         echo "集群无法停止，停止只能关闭所有节点并'备份删除'所有节点的dump.db，appendonly.aof，nodes.conf文件后启动"
     else
-        "Cluster can not stop, stop only shut down all nodes and 'backup delete' all nodes after dump.db, appendonly.aof, nodes.conf file started"
+        echo "Cluster can not stop, stop only shut down all nodes and 'backup delete' all nodes after dump.db, appendonly.aof, nodes.conf file started"
     fi
 }
 
@@ -42,11 +45,11 @@ info_redis_cluster() {
     if [ $language -eq 1 ];then
         echo "名字：redis-cluster
        
-版本：3.2.9
+依赖：redis
+
+介绍：创建redis集群
 
 作者：book
-
-介绍：创建一个redis集群
 
 提示：需要编辑加入集群的节点，至少6个点，3主3从
 
@@ -54,11 +57,11 @@ info_redis_cluster() {
     else
         echo "Name：redis-cluster
         
-version：3.2.9
-
-Author：book
+rely：redis
 
 Introduction：Create a redis cluster
+
+Author：book
 
 Prompt：Need to edit join the cluster node, at least 6 points, 3 main 3 from
 
