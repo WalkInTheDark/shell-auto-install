d3b0ab86d613536a36334d9b23fce5cca96314be
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-26 09:08:59 +0800

    Update zookeeper_cluster.sh

--- script/zookeeper_cluster.sh
+++ script/zookeeper_cluster.sh
@@ -20,6 +20,10 @@ cluster_ip=(192.168.2.108 192.168.2.109)
 
 get_zookeeper_cluster() {
     test_package zookeeper-3.5.2-alpha.tar.gz http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/zookeeper-3.5.2-alpha.tar.gz
+
+    if [ ! -n "$1" ];then
+        [ $language -eq 1 ] && echo "下载完成" || echo "Download completed"
+    fi  
 }
 
 install_zookeeper_cluster() {
@@ -27,10 +31,11 @@ install_zookeeper_cluster() {
     test_dir ${zookeeper_cluster_dir}
     test_install java-1.8.0-openjdk
     
-    get_zookeeper_cluster
-    tar -xf   package/zookeeper-3.5.2-alpha.tar.gz
+    package=`get_zookeeper_cluster 1`
+    tar -xf   package/${package}
     mv zookeeper-3.5.2-alpha ${install_dir}/${zookeeper_cluster_dir}
     
+    #配置文件
     echo "clientPort=2181
 dataDir=${install_dir}/${zookeeper_cluster_dir}/data
 syncLimit=5
@@ -39,13 +44,15 @@ initLimit=10
 dataLogDir=${install_dir}/${zookeeper_cluster_dir}
 dynamicConfigFile=${install_dir}/${zookeeper_cluster_dir}/conf/zoo.cfg.dynamic" > ${install_dir}/${zookeeper_cluster_dir}/conf/zoo.cfg
 
+    #输出配置
     d=1
     for i in `echo ${cluster_ip[*]}`
     do
         echo "server.${d}=${i}:2888:3888" >> ${install_dir}/${zookeeper_cluster_dir}/conf/zoo.cfg.dynamic
         let d++
     done
-
+    
+    #id号
     mkdir ${install_dir}/${zookeeper_cluster_dir}/data
     id=`test_id`
     echo "$id" > ${install_dir}/${zookeeper_cluster_dir}/data/myid
@@ -59,7 +66,19 @@ ${install_dir}/${zookeeper_cluster_dir}/bin/zkServer.sh" '$1' > /usr/local/bin/m
     chmod +x /usr/local/bin/man-zookeeper
 
     clear
-    [ $language -eq 1 ] && echo "zookeeper-cluster安装完毕，使用man-zookeeper 管理" || ehco "zookeeper_cluster installation is completed, Man-zookeeper management"
+     if [ $language -eq 1 ];then
+        echo "zookeeper-cluster安装成功，使用man-zookeeper管理
+        
+安装目录：${install_dir}/${zookeeper_cluster_dir}
+
+日志目录：${log_dir}/${zookeeper_cluster_dir}"
+    else
+        echo "zookeeper-cluster installed successfully, using man-zookeeper management
+        
+installation manual：${install_dir}/${zookeeper_cluster_dir}
+
+Log directory：${log_dir}/${zookeeper_cluster_dir}"
+    fi
 }
 
 remove_zookeeper_cluster() {
@@ -67,9 +86,8 @@ remove_zookeeper_cluster() {
     rm -rf /usr/local/bin/man-zookeeper
     rm -rf ${install_dir}/${zookeeper_cluster_dir}
     
-    [ $language -eq 1 ] && echo "zookeeper_cluster已卸载" || ehco "zookeeper_cluster Uninstalled"
+    [ $language -eq 1 ] && echo "zookeeper_cluster已卸载" || echo "zookeeper_cluster Uninstalled"
 }
-
 
 info_zookeeper_cluster() {
     if [ $language -eq 1 ];then
@@ -77,21 +95,21 @@ info_zookeeper_cluster() {
         
 版本：3.5.2
 
-作者：book
-
 介绍：安装zookeeper集群
+
+作者：book
 
 提示：只需要写上集群ip即可，id号将会自动创建
 
-使用：使用man-zookeeper 管理"
+使用：man-zookeeper 管理"
     else
         echo "Name：zookeeper-cluster
 
 version：3.5.2
 
-Author：book
-
 Introduction：Install zookeeper cluster
+
+Author：book
 
 Prompt：Just need to write the cluster ip can, id number will be created automatically
 
