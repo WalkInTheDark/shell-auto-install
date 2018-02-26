2bcb3661f3df087b9e477f904e16eba913432efc
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-26 08:58:25 +0800

    Update card_life.sh

--- script/card_life.sh
+++ script/card_life.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+
+
 get_card_life() {
-    [ $language -eq 1 ] && echo "不用下载" || ehco "Do not download"
+    [ $language -eq 1 ] && echo "不用下载" || echo "Do not download"
 }
 
 install_card_life() {
@@ -9,12 +11,15 @@ install_card_life() {
     chmod +x /usr/local/bin/card-life
 
     echo "card-life" >> conf/installed.txt
-    [ $language -eq 1 ] && echo "card-life安装完毕，使用card-life 开始游戏" || ehco "card-life installation is completed, use card-life Start the game"
+    
+    clear
+    [ $language -eq 1 ] && echo "card-life安装完毕，使用card-life开始游戏" || echo "card-life installation is completed, use card-life Start the game"
 }
 
 remove_card_life() {
     rm -rf /usr/local/bin/card-life
-    [ $language -eq 1 ] && echo "card-life已卸载" || ehco "card-life Uninstalled"
+    
+    [ $language -eq 1 ] && echo "card-life已卸载" || echo "card-life Uninstalled"
 }
 
 info_card_life() {
@@ -23,9 +28,9 @@ info_card_life() {
 
 版本：1.4
 
-作者：book
+介绍：抽卡人生
 
-介绍：抽卡人生shell版本，你被困在地牢里，目标是逃出去
+作者：book
 
 提示：将会生成wiki_book.ttt的一个存档，如果想开挂，修改里面数值即可。
 
@@ -35,9 +40,9 @@ info_card_life() {
         
 version：1.4
 
-Author：book
+Introduction：Card life
 
-Introduction：Shell life shell version, you are trapped in the dungeon, the goal is to escape
+Author：book
 
 Prompt：Will generate an archive of wiki_book.ttt, if you want to hang up, you can modify the value inside.
 
