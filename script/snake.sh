19781fc4e6847a619a1f80ec4fae40d4a9cecb30
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-26 09:07:42 +0800

    Update snake.sh

--- script/snake.sh
+++ script/snake.sh
@@ -1,47 +1,51 @@
 #!/usr/bin/env bash
 
-get_snake2() {
-    [ $language -eq 1 ] && echo "不用下载" || ehco "Do not download"
+
+
+get_snake() {
+    [ $language -eq 1 ] && echo "不用下载" || echo "Do not download"
 }
 
-install_snake2() {
-    cp conf/alone/snake2 /usr/local/bin/snake2
-    chmod +x /usr/local/bin/snake2
+install_snake() {
+    cp conf/alone/snake /usr/local/bin/snake
+    chmod +x /usr/local/bin/snake
 
-    echo "snake2" >> conf/installed.txt
-    [ $language -eq 1 ] && echo "snake2安装完毕，使用snake2 开始游戏" || ehco "snake2 installation is completed, use snake2 Start the game"
+    echo "snake" >> conf/installed.txt
+    
+    clear
+    [ $language -eq 1 ] && echo "snake安装完毕，使用snake开始游戏" || echo "snake installation is completed, use snake Start the game"
 }
 
-remove_snake2() {
-    rm -rf /usr/local/bin/snake2
-    [ $language -eq 1 ] && echo "snake2已卸载" || ehco "snake2 Uninstalled"
+remove_snake() {
+    rm -rf /usr/local/bin/snake
+    
+    [ $language -eq 1 ] && echo "snake已卸载" || echo "snake Uninstalled"
 }
 
-
-info_snake2() {
+info_snake() {
     if [ $language -eq 1 ];then
         echo "名字：snake
         
 版本：1.0
 
-作者：liungkejin
+介绍：贪吃蛇
 
-介绍：大家熟知的贪吃蛇
+作者：liungkejin
 
 提示：无
 
-使用：snake2命令进入游戏，jkil来吃点"
+使用：snake命令进入游戏，jkil来吃点"
     else
         echo "Name：snake
         
 version：1.0
 
-Author：liungkejin
+Introduction：snake
 
-Introduction：We all know the snake
+Author：liungkejin
 
 Prompt：none
 
-use：snake2 command into the game, jkil move to eat"
+use：snake command into the game, jkil move to eat"
     fi
 }
