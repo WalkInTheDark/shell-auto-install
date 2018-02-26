a5c50482f0927c0a7495c89181e70ac2c39a1746
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-26 09:04:35 +0800

    Update nodejs.sh

--- script/nodejs.sh
+++ script/nodejs.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
+
+
 get_nodejs() {
-    test_package node-v8.9.3-linux-x64.tar.xz http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/node-v8.9.3-linux-x64.tar.xz 32948a8ca5e6a7b69c03ec1a23b16cd2
-    if [ $language -eq 1 ];then
-        echo "node-v8.9.3-linux-x64.tar.xz 下载完成"
-    else
-        echo "node-v8.9.3-linux-x64.tar.xz Download completed"
-    fi    
+    test_package node-v8.9.3-linux-x64.tar.xz http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/node-v8.9.3-linux-x64.tar.xz
+
+    if [ ! -n "$1" ];then
+        [ $language -eq 1 ] && echo "下载完成" || echo "Download completed"
+    fi
 }
 
 install_nodejs() {
-    get_nodejs
+    package=`get_nodejs 1`
 
-    tar -xf package/node-v8.9.3-linux-x64.tar.xz
+    tar -xf package/${package}
     mv node-v8.9.3-linux-x64 /usr/local/nodejs
     
     ln -s /usr/local/nodejs/bin/node /usr/local/bin/node
     ln -s /usr/local/nodejs/bin/npm /usr/local/bin/npm
 
     echo "nodejs" >> conf/installed.txt
-    [ $language -eq 1 ] && echo "nodejs安装完毕，使用node -v 查看版本" || ehco "nodejs installation is completed，Use node -v to check the version"
+    
+    clear
+    [ $language -eq 1 ] && echo "nodejs安装完毕，使用node -v 查看版本" || echo "nodejs installation is completed，Use node -v to check the version"
 }
 
 remove_nodejs() {
@@ -27,7 +30,7 @@ remove_nodejs() {
     rm -rf /usr/local/bin/node
     rm -rf /usr/local/bin/npm
 
-    [ $language -eq 1 ] && echo "nodejs已卸载" || ehco "nodejs Uninstalled"
+    [ $language -eq 1 ] && echo "nodejs已卸载" || echo "nodejs Uninstalled"
 }
 
 
@@ -37,9 +40,9 @@ info_nodejs() {
         
 版本：8.9.3
 
-作者：book
-
 介绍：安装nodejs
+
+作者：book
 
 提示：无
 
@@ -49,9 +52,9 @@ info_nodejs() {
         
 version：8.9.3
 
-Author：book
-
 Introduction：Install nodejs
+
+Author：book
 
 Prompt：none
 
