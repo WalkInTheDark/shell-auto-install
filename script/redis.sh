f2223a5660582d03ea09deee0b105f79f68331c1
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-28 10:02:04 +0800

    Update redis.sh

--- script/redis.sh
+++ script/redis.sh
@@ -17,11 +17,7 @@ redis_dir=redis
 
 #$1有值则不显示下载完成，只显示包名
 get_redis() {
-    test_package redis-3.2.9.tar.gz http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/redis-3.2.9.tar.gz
-    
-    if [ ! -n "$1" ];then
-        [ $language -eq 1 ] && echo "下载完成" || echo "Download completed"
-    fi
+    test_package "http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/redis-3.2.9.tar.gz" "0969f42d1675a44d137f0a2e05f9ebd2"
 }
 
 install_redis() {
@@ -30,8 +26,7 @@ install_redis() {
     test_dir $redis_dir
     
     #安装服务
-    package=`get_redis 1`
-    tar -xf package/$package
+    tar -xf package/redis-3.2.9.tar.gz
     mv redis ${install_dir}/${redis_dir}
 
     #环境变量
@@ -40,24 +35,15 @@ install_redis() {
     
     #安装完成
     echo "redis" >> conf/installed.txt
-    clear
-    if [ $language -eq 1 ];then
-        echo "redis安装成功，请安装redis-port来启动一个实例
-        
+
+    test_info "redis安装成功，可安装redis-port来启动一个实例
 安装目录：${install_dir}/${redis_dir}
-
 日志目录：${log_dir}/${redis_dir}
-
-环境变量设置完毕，请退出当前终端后重新进入"
-    else
-        echo "redis installed successfully, please install redis-port to start an instance
-        
+环境变量设置完毕，请退出当前终端后重新进入" "
+redis installed successfully, You can install redis-port to start an instance
 installation manual：${install_dir}/${redis_dir}
-
 Log directory：${log_dir}/${redis_dir}
-
 Environment variable is set, please exit the current terminal and re-enter"
-    fi
 }
 
 remove_redis() {
@@ -71,33 +57,21 @@ remove_redis() {
     rm -rf ${install_dir}/${redis_dir}
 
     #卸载完成
-    [ $language -eq 1 ] && echo "redis卸载完成" || echo "redis uninstall completed"
+    test_info "redis卸载完成" "redis uninstall completed"
 }
 
 info_redis() {
-    if [ $language -eq 1 ];then
-        echo "名字：redis
-
+    test_info "
+名字：redis
 版本：3.2.9
-
 介绍：仅安装redis
-
 作者：book
-
 提示：可以修改安装路径
-
-使用：安装redis-port 来创建一个实例"
-    else
-        echo "Name：redis
-        
+使用：安装redis-port 来创建一个实例" "
+Name：redis
 version：3.2.9
-
 Introduction：Only redis is installed, but it does not start the instance
-
 Author：book
-
 Prompt：You can modify the installation path
-
 use：Install redis-port to create an instance"
-    fi
 }
