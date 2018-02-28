d5f890308998db3d126dc8ba0e665a32722e06ca
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-02-28 10:22:59 +0800

    Update mysql.sh

--- script/mysql.sh
+++ script/mysql.sh
@@ -16,14 +16,11 @@ mysql_dir=mysql
 
 
 get_mysql() {
-    test_package mysql-5.6.39-linux-glibc2.12-x86_64.tar.gz http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/mysql-5.6.39-linux-glibc2.12-x86_64.tar.gz
-
-    if [ ! -n "$1" ];then
-        [ $language -eq 1 ] && echo "下载完成" || echo "Download completed"
-    fi
+    test_package "http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/mysql-5.6.39-linux-glibc2.12-x86_64.tar.gz" "1bc406d2fe18dd877182a0bd603e7bd4"
 }
 
 install_mysql() {
+    #检测目录
     test_dir_master
     test_dir $mysql_dir
 
@@ -34,112 +31,49 @@ install_mysql() {
     groupadd mysql
     useradd -g mysql -s /sbin/nologin mysql
     
-    package=`get_mysql 1`
-    tar -xf package/$package
+    tar -xf package/mysql-5.6.39-linux-glibc2.12-x86_64.tar.gz
     mv mysql-5.6.39-linux-glibc2.12-x86_64 ${install_dir}/${mysql_dir}
     chown -R mysql:mysql ${install_dir}/${mysql_dir}
     chown -R mysql:mysql ${log_dir}/${mysql_dir}
-    
-    echo "[mysql]
-default-character-set=utf8
-socket=${install_dir}/${mysql_dir}/mysql.sock
-[mysqld]
-skip-name-resolve
-port = 3306
-socket=${install_dir}/${mysql_dir}/mysql.sock
-basedir=${install_dir}/${mysql_dir}
-datadir=${install_dir}/${mysql_dir}/data
-max_connection=200
-character-set-server=utf8
-default-storage-engine=INNODB
-lower_case_table_name=1
-max_allowed_packet=16M
-log-error=${log_dir}/${mysql_dir}/mysql.log
-pid-file=${log_dir}/${mysql_dir}/mysql.pid
-bind-address = 0.0.0.0" > /etc/my.cnf #这里改需要的配置
-    chown mysql:mysql /etc/my.cnf
 
     echo "mysql" >> conf/installed.txt #后面将不会退出
-    
-    cd ${install_dir}/${mysql_dir}
-    ./scripts/mysql_install_db --user=mysql --basedir=${install_dir}/${mysql_dir} --datadir=${install_dir}/${mysql_dir}/data &> /dev/null
-    
-    echo "[Unit]
-Description=mysql
-After=network.target remote-fs.target nss-lookup.target
-
-[Service]
-Type=forking
-ExecStart=${install_dir}/${mysql_dir}/support-files/mysql.server start
-ExecReload=${install_dir}/${mysql_dir}/support-files/mysql.server restart
-ExecStop=${install_dir}/${mysql_dir}/support-files/mysql.server stop
-PrivateTmp=true
-
-[Install]
-WantedBy=multi-user.target" > /usr/lib/systemd/system/mysql.service
-
-    systemctl daemon-reload
 
     grep 'PATH=$PATH':${install_dir}/${mysql_dir}/bin /etc/profile &> /dev/null
     [ $? -eq 0 ] || echo 'PATH=$PATH':${install_dir}/${mysql_dir}/bin >> /etc/profile
-    
-    clear
-    if [ $language -eq 1 ];then
-        echo "mysql 安装成功，请使用systemctl start mysql来启动
-        
+
+    test_info "
+mysql安装完成，可以安装mysql-single启动单点
 安装目录：${install_dir}/${mysql_dir}
-
 日志目录：${log_dir}/${mysql_dir}
-
-环境变量设置完毕，请退出当前终端后重新进入"
-    else
-        echo "mysql installed successfully, please use the systemctl start mysql to start
-        
+环境变量设置完毕，请退出当前终端后重新进入" "
+mysql installation is complete, you can install mysql-single boot a single point
 installation manual：${install_dir}/${redis_dir}
-
 Log directory：${log_dir}/${mysql_dir}
-
 Environment variable is set, please exit the current terminal and re-enter"
-    fi
 }
 
 remove_mysql() {
-    systemctl stop mysql ; systemctl disable mysql
-    rm -rf /usr/lib/systemd/system/mysql.service
-    
     hang=`grep -n 'PATH=$PATH':${install_dir}/${mysql_dir}/bin /etc/profile &> /dev/null`
     sed -i "${hang} d" /etc/profile
     
     rm -rf ${install_dir}/${mysql_dir}
     userdel -r mysql
     
-    [ $language -eq 1 ] && echo "mysql卸载完成" || echo "mysql Uninstall completed"
+    test_info "mysql卸载完成" "mysql Uninstall completed"
 }
 
 info_mysql() {
-    if [ $language -eq 1 ];then
-        echo "名字：mysql
-        
+test_info "
+名字：mysql
 版本：6.3.9
-
-介绍：安装mysql
-
+介绍：仅安装mysql
 作者：book
-
 提示：无
-
-使用：systemctl start mysql来启动"
-    else
-        echo "Name：mysql
-        
+使用：无" "
+Name：mysql
 version：6.3.9
-
-Introduction：Install mysql
-
+Introduction：Only install mysql
 Author：book
-
 Prompt：none
-
-use：systemctl start mysql to start"
-    fi
+use：none"
 }
