b7a402f3baf56f5cf000aec03439941982f27876
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-09 10:33:49 +0800

    Update mysql.sh

--- script/mysql.sh
+++ script/mysql.sh
@@ -31,6 +31,7 @@ install_mysql() {
     groupadd mysql
     useradd -g mysql -s /sbin/nologin mysql
     
+    get_mysql
     tar -xf package/mysql-5.6.39-linux-glibc2.12-x86_64.tar.gz
     mv mysql-5.6.39-linux-glibc2.12-x86_64 ${install_dir}/${mysql_dir}
     chown -R mysql:mysql ${install_dir}/${mysql_dir}
@@ -38,42 +39,60 @@ install_mysql() {
 
     echo "mysql" >> conf/installed.txt #后面将不会退出
 
-    grep 'PATH=$PATH':${install_dir}/${mysql_dir}/bin /etc/profile &> /dev/null
+    grep 'PATH=$PATH':${install_dir}/${mysql_dir}/bin /etc/profile
     [ $? -eq 0 ] || echo 'PATH=$PATH':${install_dir}/${mysql_dir}/bin >> /etc/profile
 
-    test_info "
-mysql安装完成，可以安装mysql-single启动单点
+    if [ $language -eq 1 ];then
+        echo "mysql安装完成，可以安装mysql-single启动单点
+        
 安装目录：${install_dir}/${mysql_dir}
+
 日志目录：${log_dir}/${mysql_dir}
-环境变量设置完毕，请退出当前终端后重新进入" "
-mysql installation is complete, you can install mysql-single boot a single point
+
+环境变量设置完毕，请退出当前终端后重新进入" 
+    else
+        echo "mysql installation is complete, you can install mysql-single boot a single point
+        
 installation manual：${install_dir}/${redis_dir}
+
 Log directory：${log_dir}/${mysql_dir}
+
 Environment variable is set, please exit the current terminal and re-enter"
 }
 
 remove_mysql() {
-    hang=`grep -n 'PATH=$PATH':${install_dir}/${mysql_dir}/bin /etc/profile &> /dev/null`
-    sed -i "${hang} d" /etc/profile
+    hang=`grep -n 'PATH=$PATH':${install_dir}/${mysql_dir}/bin /etc/profile | awk -F':' '{print $1}' `
+    [ ! $hang ] || sed -i "${hang} d" /etc/profile
     
     rm -rf ${install_dir}/${mysql_dir}
     userdel -r mysql
     
-    test_info "mysql卸载完成" "mysql Uninstall completed"
+    [ $language -eq 1 ] && "mysql卸载完成" || "mysql Uninstall completed"
 }
 
 info_mysql() {
-test_info "
-名字：mysql
+if [ $language -eq 1 ];then
+    echo "名字：mysql
+
 版本：6.3.9
+
 介绍：仅安装mysql
+
 作者：book
+
 提示：无
-使用：无" "
-Name：mysql
+
+使用：无"
+else
+    echo "Name：mysql
+    
 version：6.3.9
+
 Introduction：Only install mysql
+
 Author：book
+
 Prompt：none
+
 use：none"
 }
