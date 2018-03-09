426de88901d95f64391d0c4df84e3002e29433da
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-09 09:48:23 +0800

    Update process.sh

--- lib/process.sh
+++ lib/process.sh
@@ -85,8 +85,6 @@ process_capital() {
     fi
 }
 
-
-
 #一个随机端口号
 process_port() {
     shuf -i 9000-19999 -n 1
@@ -125,3 +123,46 @@ process_char(){
     stty $SAVEDSTTY
 }
 
+#集群使用，根据本地ip算出id号，统一cluster_ip，返回他在数组第几号
+process_id() {
+ 	local num=`echo ${#cluster_ip[*]}` id a=1 i e
+    	let num--
+
+	for i in `ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\."`
+	do
+		[ $a -eq 0 ] && break
+		for e in `seq 0 $num`
+		do
+            		echo ${cluster_ip[$e]} | grep $i &> /dev/null
+        		if [ $? -eq 0 ];then
+               			id=$e
+				a=0
+                		break
+        		fi
+        	done
+    	done
+	
+	let id++
+	echo $id
+}
+
+#集群使用，根据本地ip算出当前绑定ip，统一cluster_ip，返回绑定ip
+process_ip() {
+ 	local num=`echo ${#cluster_ip[*]}` ip a=1 i e
+    	let num--
+
+	for i in `ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\."`
+	do
+		[ $a -eq 0 ] && break
+		for e in `seq 0 $num`
+		do
+            		echo ${cluster_ip[$e]} | grep $i &> /dev/null
+        		if [ $? -eq 0 ];then
+               			ip=$i
+				a=0
+                		break
+        		fi
+        	done
+    	done
+	echo $ip
+}
