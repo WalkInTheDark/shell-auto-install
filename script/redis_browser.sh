46a4837775590848f949c0ab990bc63716e9f746
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 14:25:22 +0800

    Update redis_browser.sh

--- script/redis_browser.sh
+++ script/redis_browser.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#redis-browser，redis的可视化工具
 
 
 #[使用设置]
@@ -19,8 +19,6 @@ cluster_ip=(192.168.2.108:7000 192.168.2.108:7002 192.168.2.108:7004)
 
 
 get_redis_browser() {
-    bash sai.sh get nodejs
-    bash sai.sh get ruby
     test_package "http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/redis-4.0.1.gem" "a4b74c19159531d0aa4c3bf4539b1743"
     test_package "http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/redis-browser-0.5.1.gem" "dbe6a5e711dacbca46e68b10466d9da4"
 }
@@ -35,6 +33,7 @@ install_redis_browser() {
     bash sai.sh install nodejs
     bash sai.sh install ruby
     
+    get_redis_browser
     gem update —system
     gem sources —add https://gems.ruby-china.org/ —remove https://rubygems.org/
     gem sources -l
@@ -72,26 +71,21 @@ install_redis_browser() {
     sed -i "4a redis_browser_dir=$redis_browser_dir" $command
     sed -i "5a one=${cluster_ip}" $command
 
-    echo "redis-browser" >> conf/installed.txt
+    #测试
+    
     
     clear
-    if [ $language -eq 1 ];then
-    echo "redis-browser安装成功，使用man-redis-browser管理
+    echo "install ok
         
-安装目录：${install_dir}/${redis_browser_dir}
+install_dir=${install_dir}/${redis_browser_dir}
 
-日志目录：${log_dir}/${redis_browser_dir}
+log_dir=${log_dir}/${redis_browser_dir}
 
-浏览器输入 http://127.0.0.1:1212登陆"
-    else
-        echo "redis-browser installed successfully, using man-redis-browser management
-        
-installation manual：${install_dir}/${redis_browser_dir}
+bin=/usr/local/bin/man-redis-browser
 
-Log directory：${log_dir}/${redis_browser_dir}
+man-redis-browser start
 
-Browser enter http://127.0.0.1:1212 login"
-    fi
+curl http://127.0.0.1:1212"
 }
 
 remove_redis_browser() {
@@ -103,29 +97,9 @@ remove_redis_browser() {
 }
 
 info_redis_browser() {
-    if [ $language -eq 1 ];then
-        echo "名字：redis-browser
-
-版本：0.5.1
-
-介绍：安装redis-browser
-
-作者：book
-
-提示：需要修改配置文件，填写redis节点
-
-使用：使用man-redis-browser管理，浏览器输入http://127.0.0.1:1212登陆"
-    else
         echo "Name：redis-browser
         
 version：0.5.1
 
-Introduction：Install redis-browser, which is a redis visualization tool that can manipulate the database from the web
-
-Author：book
-
-Prompt：Need to modify the configuration file, fill in the redis node
-
-use：Man-redis-browser management，Browser enter http://127.0.0.1:1212 login"
-    fi
+Introduction：安装redis-browser"
 }
