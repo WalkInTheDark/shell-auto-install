7673067cb4da3695a8f98075a89ae1ff1af79045
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 11:31:30 +0800

    Update test.sh

--- lib/test.sh
+++ lib/test.sh
@@ -3,25 +3,15 @@
 
 
 
-#提示并退出脚本，$1中文，$2英文
+#提示并退出脚本，$1英文
 test_exit() {
-if [[ $language -eq 1 ]];then
     clear
     echo
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo 错误：$1
+    echo Error：$1
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     echo
     exit
-else
-    clear
-    echo
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo 错误：$2
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo
-    exit
-fi
 }
 
 
@@ -38,7 +28,7 @@ test_64bit(){
 #测试是否是root
 test_root(){
     if [[ $EUID -ne 0 ]]; then
-        test_exit "这个脚本必须以root身份运行" "This script must be run as root"
+        test_exit "This script must be run as root"
     fi
 }
 
@@ -46,7 +36,7 @@ test_root(){
 #测试网站是否正常，$1为网址
 test_www() {
     local a=`curl -o /dev/null --connect-timeout 3 -s -w "%{http_code}" $1`
-    [ $a -eq 200 ] || test_exit "无法访问外网，请重试或者调试网络" "Unable to access external network, please try again or debug network" 
+    [ $a -eq 200 ] || test_exit "Unable to access external network, please try again or debug network" 
 }
 
 
@@ -81,7 +71,7 @@ test_dir() {
     fi
 
     if [[ -d ${install_dir}/$1 ]];then
-        test_exit "${install_dir}/${1}目录已经存在，请检查安装脚本路径或手动删除目录" "$ {install_dir}/${1} directory already exists, please check the installation script path or manually delete the directory"
+        test_exit  "$ {install_dir}/${1} directory already exists, please check the installation script path or manually delete the directory"
     fi
 }
 
@@ -92,37 +82,27 @@ test_install() {
     for i in `echo $@`
     do
         rpm -q $i
-        [ $? -eq 0 ] || test_exit "${1}软件包找不到，请手动安装" "${1} software package can not find, please manually install"
+        [ $? -eq 0 ] || test_exit  "${1} software package can not find, please manually install"
     done
 }
 
-
-#扩展服务使用，$1写必须安装的服务名
-test_rely() {
-    bash sai.sh list installed | grep "^${1}"
-    [ $? -ne 0 ] && bash sai.sh install $1
-}
-
-#下载安装包，$1为网络下载地址，$2为md5值
+#下载安装包，$1为网络下载地址，$2为md5值，md5可以不写
 test_package() {
+    test_install wget
     local a=0 b=`echo ${1##*/}` c i
 
     if [ -f package/$b ];then
-        c=`md5sum package/$b | awk '{print $1}'`
-        [ "$c" == "$2" ] && a=1 || rm -rf package/$b
+        if [ ! $2 ];then
+            a=1
+        else
+            c=`md5sum package/$b | awk '{print $1}'`
+            [ "$c" == "$2" ] && a=1 || rm -rf package/$b
+        fi
     fi
     
     if [ $a -eq 0 ];then
         test_www www.baidu.com
         wget -O package/${b} $1
         test_package $1 $2 #验证
-    fi
-}
-
-
-test_remove() {
-    [ $language -eq 1 ] && read -p "确定删除[y/n]：" yn || read -p "OK to delete [y/n]：" yn
-    if [ "$yn" != "y" ];then
-       exit
     fi
 }
