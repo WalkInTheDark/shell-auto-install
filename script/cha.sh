92bedb4f4fbce9bc1c4db6759124a80ce4a05076
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 11:02:21 +0800

    Update cha.sh

--- script/cha.sh
+++ script/cha.sh
@@ -1,51 +1,29 @@
 #!/usr/bin/env bash
-
+#可以查询系统的相关信息罗列出来
+#不满意或精简，请修改脚本
 
 
 get_cha() {
-    [ $language -eq 1 ] && echo "不用下载" || echo "Do not download"
+    test_package https://raw.githubusercontent.com/goodboy23/shell-script/master/script/cha
 }
 
 install_cha() {
-    cp conf/alone/cha /usr/local/bin/cha
+    get_cha
+    cp package/cha /usr/local/bin/cha
     chmod +x /usr/local/bin/cha
 
-    echo "cha" >> conf/installed.txt
-    
     clear
-    [ $language -eq 1 ] && echo "cha安装完毕，使用cha -c 显示中文系统信息" || echo "cha installation is completed, use cha -c display Chinese system information"
-}
-
-remove_cha() {
-    rm -rf /usr/local/bin/cha
+    echo "install ok
     
-    [ $language -eq 1 ] && echo "cha已卸载" || echo "cha Uninstalled"
+dir=/usr/local/bin/cha
+    
+use cha -c display Chinese system information"
 }
 
 info_cha() {
-    if [ $language -eq 1 ];then
-        echo "名字：cha
-        
-版本：1.0
-
-介绍：显示系统信息
-
-作者：book
-
-提示：无
-
-使用：cha命令显示英文系统信息，-c显示中文"
-    else
-        echo "Name：cha
+    echo "Name：cha
         
 version：1.0
 
-Introduction：Display system information
-
-Author：book
-
-Prompt：none
-
-use：cha command to display English system information, -c display Chinese"
-    fi
+Introduction：Display system information"
 }
