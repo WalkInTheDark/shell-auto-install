212dcc2c56fc7a5dd887fec019e5f32387ce891b
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 13:55:42 +0800

    Update redis_cluster.sh

--- script/redis_cluster.sh
+++ script/redis_cluster.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-
+#此脚本将创建集群，需要设置端口
 
 
 #[使用设置]
+
+#可使用非sai安装的redis
+#install_dir=
+
+#redis_dir=
 
 #所有要加入集群的节点，前一半节点皆为主
 cluster_ip="127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005"
@@ -10,13 +15,7 @@ cluster_ip="127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.
 #默认1主1从，设置2就是1主2从
 node=1
 
-#非0则不检测依赖
-rely=0
 
-
-
-#加载它的依赖
-source script/redis.sh
 
 get_redis_cluster() {
     get_redis
@@ -24,52 +23,26 @@ get_redis_cluster() {
 }
 
 install_redis_cluster() {  
-    [ $rely -eq 0 ] && test_rely redis
+    [ ! $install_dir ] || source script/redis.sh
 
     get_redis_cluster
     test_install ruby-devel rubygems rpm-build
-    bash sai.sh install ruby #这个手动装好点
+    bash sai.sh install ruby
     gem install package/redis-4.0.1.gem
     
+    #启动
     ${install_dir}/${redis_dir}/src/redis-trib.rb create --replicas ${node} ${cluster_ip}
     
+    #测试
+    
     clear
-    [ $language -eq 1 ] && echo "redis集群已启动完成" || echo "redis cluster has started to complete"
-}
-
-
-remove_redis_cluster() {
-    if [ $language -eq 1 ];then
-        echo "集群无法停止，停止只能关闭所有节点并'备份删除'所有节点的dump.db，appendonly.aof，nodes.conf文件后启动"
-    else
-        echo "Cluster can not stop, stop only shut down all nodes and 'backup delete' all nodes after dump.db, appendonly.aof, nodes.conf file started"
-    fi
+    echo "install ok"
 }
 
 info_redis_cluster() {
-    if [ $language -eq 1 ];then
-        echo "名字：redis-cluster
-       
-依赖：redis
-
-介绍：创建redis集群
-
-作者：book
-
-提示：需要编辑加入集群的节点，至少6个点，3主3从
-
-使用：集群使用和测试方法敬请百度"
-    else
-        echo "Name：redis-cluster
+    echo "Name：redis-cluster
         
 rely：redis
 
-Introduction：Create a redis cluster
-
-Author：book
-
-Prompt：Need to edit join the cluster node, at least 6 points, 3 main 3 from
-
-use：Baidu use and test methods please Baidu"
-    fi
+Introduction：创建集群"
 }
