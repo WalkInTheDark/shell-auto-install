d26b66517d9e66e02757b57fbd63afbbf0d258b9
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 11:24:31 +0800

    Update sai.sh

--- sai.sh
+++ sai.sh
@@ -15,25 +15,6 @@ editor=vi
 
 
 
-#中文帮助
-help_cn() {
-    echo "提示：所有服务的安装均为默认设置！若自定义安装位置或其它设置请 edit 服务名
-当前版本1.5.7
-
-install httpd      安装 httpd
-remove  httpd      卸载 httpd
-get     httpd      下载 httpd 所需要的包
-info    httpd      查询 httpd 详细信息
-edit    httpd      编辑 httpd 进行自定义设置
-
-list               列出 支持  的脚本
-list    httpd      列出 httpd 相关脚本
-list    installed  列出 所有  已安装脚本
-
-lang    1          设置 语言  为中文
-lang    2          设置 语言  为英文"
-}
-
 #英文帮助
 help_en() {
     echo "Tip: All services are installed by default! If you customize the installation location or other settings, please edit the service name
@@ -47,10 +28,7 @@ edit	httpd      Edit         httpd for custom settings
 
 list               List supported scripts
 list    httpd      List httpd related scripts
-list    installed  Lists all installed scripts
-
-lang    1          Set the language to Chinese
-lang    2          Set the language to English"
+list    installed  Lists all installed scripts"
 }
 
 #生成list表
@@ -70,31 +48,10 @@ list_generate() {
         name=`echo $list |awk -F: '{print $1}'`
         version=`echo $list |awk -F: '{print $2}'`
         intr=`echo $list |awk -F: '{print $3}'`
-        awk 'BEGIN{printf "%-20s%-20s%-20s\n","'"$name"'","'"$version"'","'"$intr"'";}' >> conf/${server_name}
-        echo " " >> conf/${server_name}
+        awk 'BEGIN{printf "%-20s%-20s%-20s\n","'"$name"'","'"$version"'","'"$intr"'";}' >> conf/list.txt
+        echo " " >> conf/list.txt
     done < conf/b.txt
     rm -rf conf/a.txt conf/b.txt
-}
-
-#将已安装记录从list表中查找
-list_all() {
-    if [ "$1" == "installed" ];then
-            grep "^${i}" conf/list.txt | head -1
-    else
-        [ $language -eq 1 ] && echo "$1 相关脚本：" || echo "$1 Related script："
-        grep "^$1" conf/${server_name}
-    fi
-}
-
-#设置语言
-yuyan() {
-    if [ $1 -eq 1 ];then
-        echo "1" > conf/lang.txt
-    elif [ $1 -eq 2 ];then
-        echo "2" > conf/lang.txt
-    else
-        test_exit "选项错误" "Wrong option"
-    fi
 }
 
 #对于服务的操作
@@ -105,20 +62,9 @@ server() {
     if [ -f script/${a}.sh ];then
         source script/${a}.sh
         if [ "$1" == "install" ];then
-            grep "^${1}" conf/installed.txt
-            [ $? -eq 0 ] || grep "^${a}" conf/installed.txt
-            [ $? -eq 0 ]&& test_exit "${a}已安装" "${a} is already installed"
-            
-            [ $language -eq 1 ] && echo "正在安装${2}，安装中出现错误，将会退出并给出解决办法" || echo "Installing $ {2}, an error occurred during installation, it will exit and give a solution"
+            [ $language -eq 1 ] && echo "Installing $ {2}, an error occurred during installation, it will exit and give a solution"
             sleep 3
             install_${a}
-        elif [ "$1" == "remove" ];then
-            test_remove
-        
-            sed -i "/^${a}/d" conf/installed.txt
-            sed -i '/^$/d' conf/installed.txt #删除空行
-            
-            remove_${a}
         elif [ "$1" == "get" ];then
             get_${a}
         elif [ "$1" == "info" ];then   
@@ -126,32 +72,31 @@ server() {
         elif [ "$1" == "edit" ];then
             $editor script/${a}.sh
         else
-            help_all
+            help_en
         fi
     else
-        test_exit "没有这个服务" "Without this service"
+        test_exit "Without this service"
     fi
 }
 
 
 
 #主体
-server_name=list_${language}.txt
-language=`cat conf/lang.txt`
-source conf/lib.sh
+for i in `ls lib/*`
+do
+    source $i
+done
 
 if [ $# -eq 0 ];then
-    [ $language -eq 1 ] && help_cn || help_en 
+    help_en
 elif [ $# -eq 1 ];then
     if [ "$1" == "list" ];then
-        [ -f conf/${server_name} ] || list_generate
-        cat conf/${server_name}
+        [ -f conf/list.txt ] || list_generate
+        cat conf/list.txt
     fi
 elif [ $# -eq 2 ];then
     if [ "$1" == "list" ];then
-        list_all $2
-    elif [ "$1" == "lang" ];then
-        yuyan $2
+        echo "$1 Related script：" && grep "^$2" conf/${server_name}
     else
         server $1 $2
     fi
