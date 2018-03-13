d516ae07cc055c82557f58d11a9bc66625677f9c
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 11:02:00 +0800

    Update card_life.sh

--- script/card_life.sh
+++ script/card_life.sh
@@ -1,51 +1,31 @@
 #!/usr/bin/env bash
+#抽卡人生小游戏，当前最快47天通关。
+#启动将生成一个存档，不建议改存档。
+#攻略：最好是过天数，不花任何法力买卡牌，然后10连抽
 
 
 
 get_card_life() {
-    [ $language -eq 1 ] && echo "不用下载" || echo "Do not download"
+    test_package "https://raw.githubusercontent.com/goodboy23/shell-script/master/script/card-life"
 }
 
 install_card_life() {
-    cp conf/alone/card-life /usr/local/bin/card-life
+    get_card_life
+    cp package/card-life /usr/local/bin/card-life
     chmod +x /usr/local/bin/card-life
 
-    echo "card-life" >> conf/installed.txt
-    
     clear
-    [ $language -eq 1 ] && echo "card-life安装完毕，使用card-life开始游戏" || echo "card-life installation is completed, use card-life Start the game"
-}
-
-remove_card_life() {
-    rm -rf /usr/local/bin/card-life
+    echo "install ok
     
-    [ $language -eq 1 ] && echo "card-life已卸载" || echo "card-life Uninstalled"
+dir=/usr/local/bin/card-life
+
+card-life starts the game"
 }
 
 info_card_life() {
-    if [ $language -eq 1 ];then
-        echo "名字：card-life
-
-版本：1.4
-
-介绍：抽卡人生
-
-作者：book
-
-提示：将会生成wiki_book.ttt的一个存档，如果想开挂，修改里面数值即可。
-
-使用：card-life进入游戏，输入1-6来购买卡牌"
-    else
         echo "Name：card-life
-        
-version：1.4
 
-Introduction：Card life
+Version：1.4
 
-Author：book
-
-Prompt：Will generate an archive of wiki_book.ttt, if you want to hang up, you can modify the value inside.
-
-use：card-life into the game, enter 1-6 to buy cards"
-    fi
+Introduce：抽卡人生"
 }
