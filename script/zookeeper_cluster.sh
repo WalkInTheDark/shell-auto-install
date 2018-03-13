32321a4d8eee52ac8a6d6a8bc19f0ce816a96588
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 15:02:48 +0800

    Update zookeeper_cluster.sh

--- script/zookeeper_cluster.sh
+++ script/zookeeper_cluster.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-
+#设置完毕后，再每个节点上安装此脚本
 
 
 #[使用设置]
 
-#主目录，相当于/usr/local
+#可自定义
 #install_dir=
 
-#日志主目录，相当于/var/log
 #log_dir=
 
-#服务目录名
-zookeeper_cluster_dir=zookeeper
+#zookeeper_dir
 
 #集群所有节点的ip
 cluster_ip=(192.168.2.108 192.168.2.109)
@@ -19,46 +17,36 @@ cluster_ip=(192.168.2.108 192.168.2.109)
 
 
 get_zookeeper_cluster() {
-    test_package zookeeper-3.5.2-alpha.tar.gz http://shell-auto-install.oss-cn-zhangjiakou.aliyuncs.com/package/zookeeper-3.5.2-alpha.tar.gz
-
-    if [ ! -n "$1" ];then
-        [ $language -eq 1 ] && echo "下载完成" || echo "Download completed"
-    fi  
+    echo "Do not download"
 }
 
 install_zookeeper_cluster() {
-    test_dir_master
-    test_dir ${zookeeper_cluster_dir}
-    test_install java-1.8.0-openjdk
-    
-    package=`get_zookeeper_cluster 1`
-    tar -xf   package/${package}
-    mv zookeeper-3.5.2-alpha ${install_dir}/${zookeeper_cluster_dir}
-    
+    [ ! $install_dir] || source script/zookeeper.sh
+
     #配置文件
     echo "clientPort=2181
-dataDir=${install_dir}/${zookeeper_cluster_dir}/data
+dataDir=${install_dir}/${zookeeper_dir}/data
 syncLimit=5
 tickTime=2000
 initLimit=10
-dataLogDir=${install_dir}/${zookeeper_cluster_dir}
-dynamicConfigFile=${install_dir}/${zookeeper_cluster_dir}/conf/zoo.cfg.dynamic" > ${install_dir}/${zookeeper_cluster_dir}/conf/zoo.cfg
+dataLogDir=${install_dir}/${zookeeper_dir}
+dynamicConfigFile=${install_dir}/${zookeeper_dir}/conf/zoo.cfg.dynamic" > ${install_dir}/${zookeeper_dir}/conf/zoo.cfg
 
     #输出配置
     d=1
     for i in `echo ${cluster_ip[*]}`
     do
-        echo "server.${d}=${i}:2888:3888" >> ${install_dir}/${zookeeper_cluster_dir}/conf/zoo.cfg.dynamic
+        echo "server.${d}=${i}:2888:3888" >> ${install_dir}/${zookeeper_dir}/conf/zoo.cfg.dynamic
         let d++
     done
     
     #id号
-    mkdir ${install_dir}/${zookeeper_cluster_dir}/data
+    mkdir ${install_dir}/${zookeeper_dir}/data
     id=`process_id`
-    echo "$id" > ${install_dir}/${zookeeper_cluster_dir}/data/myid
+    echo "$id" > ${install_dir}/${zookeeper_dir}/data/myid
     
     #监听ipv4，默认ipv6
-    sed -i '150c "-Dzookeeper.log.file=${ZOO_LOG_FILE}" "-Djava.net.preferIPv4Stack=true"  "-Dzookeeper.root.logger=${ZOO_LOG4J_PROP}" \/' ${install_dir}/${zookeeper_cluster_dir}/bin/zkSserver.sh
+    sed -i '150c "-Dzookeeper.log.file=${ZOO_LOG_FILE}" "-Djava.net.preferIPv4Stack=true"  "-Dzookeeper.root.logger=${ZOO_LOG4J_PROP}" \/' ${install_dir}/${zookeeper_dir}/bin/zkSserver.sh
 
     
     echo "#!/bin/bash
@@ -66,53 +54,17 @@ ${install_dir}/${zookeeper_cluster_dir}/bin/zkServer.sh" '$1' > /usr/local/bin/m
     chmod +x /usr/local/bin/man-zookeeper
 
     clear
-     if [ $language -eq 1 ];then
-        echo "zookeeper-cluster安装成功，使用man-zookeeper管理
-        
-安装目录：${install_dir}/${zookeeper_cluster_dir}
+    echo "install ok
 
-日志目录：${log_dir}/${zookeeper_cluster_dir}"
-    else
-        echo "zookeeper-cluster installed successfully, using man-zookeeper management
-        
-installation manual：${install_dir}/${zookeeper_cluster_dir}
+bin=/usr/local/bin/man-zookeeper
 
-Log directory：${log_dir}/${zookeeper_cluster_dir}"
-    fi
-}
-
-remove_zookeeper_cluster() {
-    man-zookeeper stop
-    rm -rf /usr/local/bin/man-zookeeper
-    rm -rf ${install_dir}/${zookeeper_cluster_dir}
-    
-    [ $language -eq 1 ] && echo "zookeeper_cluster已卸载" || echo "zookeeper_cluster Uninstalled"
+man-zookeeper start"
 }
 
 info_zookeeper_cluster() {
-    if [ $language -eq 1 ];then
-        echo "名字：zookeeper-cluster
-        
-版本：3.5.2
+    echo "Name：zookeeper-cluster
 
-介绍：安装zookeeper集群
+version：zookeeper
 
-作者：book
-
-提示：只需要写上集群ip即可，id号将会自动创建
-
-使用：man-zookeeper 管理"
-    else
-        echo "Name：zookeeper-cluster
-
-version：3.5.2
-
-Introduction：Install zookeeper cluster
-
-Author：book
-
-Prompt：Just need to write the cluster ip can, id number will be created automatically
-
-use：Man-zookeeper management"
-    fi
+Introduction：安装zookeeper集群"
 }
