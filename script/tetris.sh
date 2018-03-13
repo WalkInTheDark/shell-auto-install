4ba730f3d378783829eb731c76cff20b8f24a25a
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 11:03:39 +0800

    Update tetris.sh

--- script/tetris.sh
+++ script/tetris.sh
@@ -1,51 +1,29 @@
 #!/usr/bin/env bash
-
+#俄罗斯方块，代码写的很工整
+#小游戏也写的很棒
 
 
 get_tetris() {
-    [ $language -eq 1 ] && echo "不用下载" || echo "Do not download"
+    test_package https://raw.githubusercontent.com/goodboy23/shell-script/master/script/tetris
 }
 
 install_tetris() {
+    get_tetris
     cp conf/alone/tetris /usr/local/bin/tetris
     chmod +x /usr/local/bin/tetris
 
-    echo "tetris" >> conf/installed.txt
-    
     clear
-    [ $language -eq 1 ] && echo "tetris安装完毕，使用tetris 开始游戏" || echo "tetris installation is completed, use tetris Start the game"
-}
+    echo "install ok
 
-remove_tetris() {
-    rm -rf /usr/local/bin/tetris
-    
-    [ $language -eq 1 ] && echo "tetris 已卸载" || echo "tetris Uninstalled"
+dir=/usr/local/bin/tetris
+
+tetris installation is completed, use tetris Start the game"
 }
 
 info_tetris() {
-    if [ $language -eq 1 ];then
-        echo "名字：tetris
-        
-版本：1.0
-
-介绍：俄罗斯方块
-
-作者：未知
-
-提示：无
-
-使用：tetris命令进入后即可开始游戏"
-    else
-        echo "Name：tetris
+    echo "Name：tetris
         
 version：1.0
 
-Introduction：Tetris
-
-Author：unknown
-
-Prompt：none
-
-use：tetris command to enter the game to start"
-    fi
+Introduction：Tetris"
 }
