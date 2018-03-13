716d73b5ccdca2e42d61c066c9d3aed87b699f6c
goodboy23 <31239628+goodboy23@users.noreply.github.com>
2018-03-13 11:13:27 +0800

    Update nodejs.sh

--- script/nodejs.sh
+++ script/nodejs.sh
@@ -11,48 +11,28 @@ install_nodejs() {
     tar -xf package/${package}
     mv node-v8.9.3-linux-x64 /usr/local/nodejs
     
+    #链接
     ln -s /usr/local/nodejs/bin/node /usr/local/bin/node
     ln -s /usr/local/nodejs/bin/npm /usr/local/bin/npm
 
-    echo "nodejs" >> conf/installed.txt
+    #对结果进行测试
+    node -v
+    [ $? -eq 0 ] || test_exit "Installation failed, please check the installation script"
     
     clear
-    [ $language -eq 1 ] && echo "nodejs安装完毕，使用node -v 查看版本" || echo "nodejs installation is completed，Use node -v to check the version"
+    echo "install ok
+
+install_dir=/usr/local/nodejs
+
+bin_dir=/usr/local/bin/node
+
+bin_dir=/usr/local/bin/npm"
 }
-
-remove_nodejs() {
-    rm -rf /usr/local/nodejs
-    rm -rf /usr/local/bin/node
-    rm -rf /usr/local/bin/npm
-
-    [ $language -eq 1 ] && echo "nodejs已卸载" || echo "nodejs Uninstalled"
-}
-
 
 info_nodejs() {
-    if [ $language -eq 1 ];then
-        echo "名字：nodejs
-        
-版本：8.9.3
-
-介绍：安装nodejs
-
-作者：book
-
-提示：无
-
-使用：无"
-    else
-        echo "Name：nodejs
+    echo "Name：nodejs
         
 version：8.9.3
 
-Introduction：Install nodejs
-
-Author：book
-
-Prompt：none
-
-use：none"
-    fi
+Introduction：安装nodejs"
 }
